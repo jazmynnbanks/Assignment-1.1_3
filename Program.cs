f3f909d84f6bc6838c11dbd2aaa56288f2391473
jazmy <jazmy@JAZ-LAPTOP>
2025-04-07 17:21:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Write a C# program to print the result of dividing two numbers. Print the quotient and remainder as well. Get the input from the user. //
+
+Console.WriteLine("Please input value number 1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please input value number 2:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 / num2;
+
+Console.WriteLine("The quotient is " + result);
+Console.WriteLine("The remainder is " + num1 % num2);
